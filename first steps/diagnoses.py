3e205108a029ae312a8b6f2be49ac71f01d8a33e
andreslilloortiz <154079318+andreslilloortiz@users.noreply.github.com>
2026-02-13 17:32:24 +0100

    add non-MH-diagnoses (english and spanish)

--- first steps/diagnoses.py
+++ first steps/diagnoses.py
@@ -1,4 +1,4 @@
-diagnoses_EN = [
+MH_diagnoses_EN = [
     "depression",
     "bipolar disorder",
     "anxiety",
@@ -12,7 +12,7 @@ diagnoses_EN = [
     "schizophrenia"
 ]
 
-diagnoses_ES = [
+MH_diagnoses_ES = [
     "depresión",
     "trastorno bipolar",
     "ansiedad",
@@ -24,4 +24,32 @@ diagnoses_ES = [
     "psicosis",
     "trastorno límite de la personalidad",
     "esquizofrenia"
+]
+
+non_MH_diagnoses_EN = [
+    "heart disease",
+    "cancer",
+    "stroke",
+    "respiratory disease",
+    "injuries",
+    "diabetes",
+    "Alzheimer’s disease",
+    "influenza",
+    "pneumonia",
+    "kidney disease",
+    "septicemia"
+]
+
+non_MH_diagnoses_ES = [
+    "enfermedad cardíaca",
+    "cáncer",
+    "accidente cerebrovascular",
+    "enfermedad respiratoria",
+    "lesiones",
+    "diabetes",
+    "enfermedad de Alzheimer",
+    "influenza",
+    "neumonía",
+    "enfermedad renal",
+    "septicemia"
 ]
