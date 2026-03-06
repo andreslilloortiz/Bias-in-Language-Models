159c74565afd41e323a030be499ebe93e76270b5
andreslilloortiz <154079318+andreslilloortiz@users.noreply.github.com>
2026-03-06 19:04:48 +0100

    LLMs bias detection: Add llama3 and mentallama models to the experiment matrix

--- LLMs bias detection/src/experiments_matrix.py
+++ LLMs bias detection/src/experiments_matrix.py
@@ -11,5 +11,23 @@ experiments = [
         "health_type": "MH",
         "diagnoses": diagnoses.MH_diagnoses_ES,
         "templates": prompts_HAPA.templates_ES
+    },
+    # Llama3, EN, MH
+    {
+        "model": models.Llama3,
+        "model_name": "Llama3",
+        "language": "EN",
+        "health_type": "MH",
+        "diagnoses": diagnoses.MH_diagnoses_EN,
+        "templates": prompts_HAPA.templates_EN
+    },
+    # MentaLLaMA, EN, MH
+    {
+        "model": models.MentaLLaMA,
+        "model_name": "MentaLLaMA",
+        "language": "EN",
+        "health_type": "MH",
+        "diagnoses": diagnoses.MH_diagnoses_EN,
+        "templates": prompts_HAPA.templates_EN
     }
 ]
